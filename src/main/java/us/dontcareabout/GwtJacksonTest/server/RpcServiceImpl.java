256dc90b980eeb351b6c2bef0690884c254a12fa
MontyPan <monty.pan@dontcareabout.us>
2019-06-02 13:14:41 +0800

    Init by JISS

---
+++ src/main/java/us/dontcareabout/GwtJacksonTest/server/RpcServiceImpl.java
@@ -0,0 +1,9 @@
+package us.dontcareabout.GwtJacksonTest.server;
+
+import us.dontcareabout.GwtJacksonTest.client.RpcService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class RpcServiceImpl extends RemoteServiceServlet implements RpcService {
+	private static final long serialVersionUID = 1L;
+
+}
