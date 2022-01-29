13a965c3f95c182c0bc6bf6043aeeaad7fce1282
MontyPan <monty.pan@dontcareabout.us>
2022-01-30 00:23:16 +0800

    receiveNormal() client 實做

--- src/main/java/us/dontcareabout/GwtJacksonTest/client/DataCenter.java
+++ src/main/java/us/dontcareabout/GwtJacksonTest/client/DataCenter.java
@@ -3,8 +3,10 @@ package us.dontcareabout.GwtJacksonTest.client;
 import com.github.nmorel.gwtjackson.client.ObjectMapper;
 import com.google.gwt.core.client.GWT;
 
+import us.dontcareabout.GwtJacksonTest.client.util.gf.RestBuilder;
 import us.dontcareabout.GwtJacksonTest.shared.Fail;
 import us.dontcareabout.GwtJacksonTest.shared.Normal;
+import us.dontcareabout.gwt.client.Console;
 
 public class DataCenter {
 	interface NormalMapper extends ObjectMapper<Normal> {}
@@ -13,4 +15,10 @@ public class DataCenter {
 	interface FailMapper extends ObjectMapper<Fail>{}
 	static FailMapper failMapper = GWT.create(FailMapper.class);
 
+	public static void receiveNormal() {
+		new RestBuilder("api/receiveNormal").post().request(
+			mapper.write(Normal.sample),
+			(req, resp) -> Console.log("receiveNormal() : " + resp.getText())
+		);
+	}
 }
