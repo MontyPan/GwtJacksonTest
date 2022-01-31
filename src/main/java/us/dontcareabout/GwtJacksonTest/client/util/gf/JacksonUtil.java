004138c04fef98c8d9bee454ecf78749ecc84bd0
MontyPan <monty.pan@dontcareabout.us>
2022-01-31 16:55:34 +0800

    增加：JacksonUtil.read()

---
+++ src/main/java/us/dontcareabout/GwtJacksonTest/client/util/gf/JacksonUtil.java
@@ -0,0 +1,26 @@
+package us.dontcareabout.GwtJacksonTest.client.util.gf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.nmorel.gwtjackson.client.ObjectMapper;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.core.client.JsonUtils;
+
+public class JacksonUtil {
+	/**
+	 * 這是為了不多製造一個 ObjectMapper<List<T>> 而搞出來的東西。
+	 */
+	public static <T> List<T> read(String json, ObjectMapper<T> mapper) {
+		JsArray<JavaScriptObject> jsArray = JsonUtils.safeEval(json);
+		ArrayList<T> result = new ArrayList<>();
+
+		for (int i = 0; i < jsArray.length(); i++) {
+			//把每個 element 再變回 json，這樣 mapper 就能處理了，愚蠢但有效的招式 XD
+			result.add(mapper.read(JsonUtils.stringify(jsArray.get(i))));
+		}
+
+		return result;
+	}
+}
